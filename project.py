21273dbf7446157477e9f6e766f7260aefb1b52c
ethan <silver58671@outlook.com>
2023-09-06 19:57:44 -0400

    add set_code function

--- project.py
+++ project.py
@@ -1,8 +1,17 @@
 import random
 
+CHOICES = ['🔴', '🟠', '🟡', '🟢', '🔵', '🟣' ]
+
 def main():
-    ...
+    code = set_code()
 
 # Computer sets the code the user has to guess
 def set_code():
-    ...
+    code = ''
+    for v in range(4):
+        code = code + random.choice(CHOICES)
+    return code
+
+
+if __name__ == '__main__':
+    main()
