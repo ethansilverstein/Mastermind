6ae4ce04a3994c8be023086f1c1b6d47f35c4689
ethansilverstein <silver58671@outlook.com>
2023-09-07 22:14:35 -0400

    add a test

--- project.py
+++ project.py
@@ -1,18 +1,19 @@
 import random
 import sys
 
-CHOICES = ['R', 'O', 'Y', 'G', 'B', 'P']
+CHOICES = ["R", "O", "Y", "G", "B", "P"]
+
 
 def main():
     game = True
     count = 0
-    
+
     print(introduction())
     code = set_code()
     while game:
         try:
             count += 1
-            guess = input('Input your guess here: ').strip().upper()
+            guess = input("Input your guess here: ").strip().upper()
             if guess == code:
                 count = 0
                 print(win_message())
@@ -24,18 +25,17 @@ def main():
             print(check_code(code, guess))
         except ValueError:
             count -= 1
-            print('Enter a valid code')
+            print("Enter a valid code")
             pass
-    
-    
 
 
 # Computer sets the code the user has to guess
 def set_code():
-    code = ''
+    code = ""
     for _ in range(4):
         code += random.choice(CHOICES)
     return code
+
 
 def check_code(code, guess):
     checked = []
@@ -51,8 +51,9 @@ def check_code(code, guess):
             if g not in checked:
                 white += 1
             code_copy.remove(g)
-                
+
     return f'You have {black} colors that are in the code and are in the right position.\nYou have {white} colors that are in the code but are in the wrong position.'
+
 
 # Checks if code is valid
 def valid(code, guess):
@@ -61,19 +62,19 @@ def valid(code, guess):
             raise ValueError
         if len(guess) != len(code):
             raise ValueError
-        
-
 
 
 def introduction():
     return 'Welcome to Mastermind!\nAs the codemaker, the computer will set a random code made up of four different colors.\nThe colors to choose from are: "🔴 (R)", "🟠 (O)", "🟡 (Y)", "🟢 (G)", "🔵 (B)", and "🟣 (P)"\nEnter the letters, not the emojis\nAs the codebreaker, you must try to break the code!\nComputer will provide hints after every guess.\nGuess the code within 10 tries to become the mastermind'
 
+
 def win_message():
-    return 'You managed to break the code! You are the mastermind!'
+    return "You managed to break the code! You are the mastermind!"
+
 
 def lose_message():
-    return 'You have lost to the codemaker :('
+    return "You have lost to the codemaker :("
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- test_project.py
+++ test_project.py
@@ -1,4 +1,11 @@
+import pytest
+from project import check_code, set_code, valid
+
 def test_check_code():
-    ...
+    assert check_code('RRRR', 'RRRY') == 'You have 3 colors that are in the code and are in the right position.\nYou have 0 colors that are in the code but are in the wrong position.'
+
 def test_set_code():
+    ...
+
+def test_valid():
     ...
